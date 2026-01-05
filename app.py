ecb4c87d94666d87a85f7397d6f234d1cd949d5b
uyenuyen2409 <tdkhanhuyen2006@gmail.com>
2026-01-05 19:37:55 +0700

    sua lai

--- app.py
+++ app.py
@@ -11,5 +11,4 @@ def load_interface(filename):
     return render_template(f'interface/{filename}')
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run(host='0.0.0.0', port=5000, debug=True)
